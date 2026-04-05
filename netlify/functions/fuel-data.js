a7b9a645943fc4c59a37a76bd79fbfea39d6331d
russ-crypto <russ@bakersrecovery.co.uk>
2026-04-05 21:57:57 +0100

    Update fuel-data.js

--- netlify/functions/fuel-data.js
+++ netlify/functions/fuel-data.js
@@ -1,10 +1,40 @@
 exports.handler = async function () {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      ok: true,
-      message: "Function is working"
-    })
-  };
+  try {
+    // Get access token
+    const tokenRes = await fetch(process.env.TOKEN_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET
+      })
+    });
+
+    const tokenJson = await tokenRes.json();
+    const accessToken = tokenJson.access_token;
+
+    // Get fuel data
+    const fuelRes = await fetch(process.env.API_URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+
+    const fuelJson = await fuelRes.json();
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(fuelJson)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
 };
